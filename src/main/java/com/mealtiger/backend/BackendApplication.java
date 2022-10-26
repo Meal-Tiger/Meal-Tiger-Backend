b9abb4387d36f54b35cec0a71cddfa932b105842
Uggah <luccagreschner@gmail.com>
2022-10-26 10:59:15 +0200

    Add config integrity check for database configuration

--- src/main/java/com/mealtiger/backend/BackendApplication.java
+++ src/main/java/com/mealtiger/backend/BackendApplication.java
@@ -4,10 +4,13 @@ import com.mealtiger.backend.configuration.ConfigLoader;
 import com.mealtiger.backend.configuration.configs.Config;
 import com.mealtiger.backend.configuration.configs.MainConfig;
 import com.mealtiger.backend.database.repository.RecipeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * This is the main class. Its main method is run to start the application.
@@ -16,6 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
+
+	private static ConfigurableApplicationContext applicationContext;
 
 	@Autowired
 	private RecipeRepository repository;
@@ -29,7 +36,11 @@ public class BackendApplication implements CommandLineRunner {
 			config = new MainConfig();
 		}
 
-		SpringApplication.run(BackendApplication.class, args);
+		if (((MainConfig) config).getMongoConnectionString().length() == 0) {
+			log.info("Database connection string is not defined. Please use the config file main.yml to configure!");
+		} else {
+			applicationContext = SpringApplication.run(BackendApplication.class, args);
+		}
 	}
 
 	@Override
