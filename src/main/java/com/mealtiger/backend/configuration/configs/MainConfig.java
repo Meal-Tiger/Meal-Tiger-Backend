05cec14e9597053c6bae5f2ed7fb515efbcba7b2
Uggah <luccagreschner@gmail.com>
2022-10-26 11:34:51 +0200

    Add database parameter to MainConfig

--- src/main/java/com/mealtiger/backend/configuration/configs/MainConfig.java
+++ src/main/java/com/mealtiger/backend/configuration/configs/MainConfig.java
@@ -2,7 +2,6 @@ package com.mealtiger.backend.configuration.configs;
 
 /**
  * Main config file.
- *
  * Properties are represented as non-static fields. Categories can be done by nesting the properties in static nested classes.
  */
 public class MainConfig implements Config {
@@ -22,11 +21,23 @@ public class MainConfig implements Config {
         return database.mongoDBURL;
     }
 
+    public String getMongoDatabase() {
+        return database.database;
+    }
+
     static class Database {
+        /**
+         * MongoDB Connection String used to connect to the database.
+         *
+         * @see <a href=https://www.mongodb.com/docs/manual/reference/connection-string/>MongoDB Reference Documentation</a>
+         */
         private final String mongoDBURL;
+
+        private final String database;
 
         Database() {
             mongoDBURL = "";
+            database = "MealTigerDB";
         }
 
     }
