80e457f5d2b7995ac917283d1cb99ad3e0019019
Uggah <luccagreschner@gmail.com>
2022-10-30 21:56:33 +0100

    Exclude SecurityAutoConfiguration to omit the login screen Spring automatically presents.

--- src/main/java/com/mealtiger/backend/BackendApplication.java
+++ src/main/java/com/mealtiger/backend/BackendApplication.java
@@ -3,13 +3,12 @@ package com.mealtiger.backend;
 import com.mealtiger.backend.configuration.ConfigLoader;
 import com.mealtiger.backend.configuration.configs.Config;
 import com.mealtiger.backend.configuration.configs.MainConfig;
-import com.mealtiger.backend.database.repository.RecipeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -17,15 +16,12 @@ import org.springframework.context.ConfigurableApplicationContext;
  *
  * @author Lucca Greschner, Sebastian Maier
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackendApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
 
 	private static ConfigurableApplicationContext applicationContext;
-
-	@Autowired
-	private RecipeRepository repository;
 
 	public static void main(String[] args) {
 		Config config;
@@ -45,7 +41,6 @@ public class BackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-
 	}
 
 }
