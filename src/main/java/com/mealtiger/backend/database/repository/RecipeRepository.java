4f1f4af697e0e0bf7f67c3e1136c175b1d8d4dad
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2022-11-08 16:10:56 +0100

    Implement querying database for title containing given string

--- src/main/java/com/mealtiger/backend/database/repository/RecipeRepository.java
+++ src/main/java/com/mealtiger/backend/database/repository/RecipeRepository.java
@@ -1,13 +1,26 @@
 package com.mealtiger.backend.database.repository;
 
 import com.mealtiger.backend.database.model.recipe.Recipe;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * This interface serves as a MongoRepository for storing data in the mongoDB database specified in MongoClientConfiguration
  */
+@Repository
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
 
+    /**
+     * Finds all recipes whose titles contain the given string - case-insensitive.
+     */
+    List<Recipe> findRecipesByTitleContainingIgnoreCase(String title, Pageable pageable);
 
+    /**
+     * Finds all recipes whose titles contain the given string - case-sensitive.
+     */
+    List<Recipe> findRecipesByTitleContaining(String title, Pageable pageable);
 
 }
---
+++ src/test/java/com/mealtiger/backend/database/repository/RecipeRepositoryTest.java
@@ -0,0 +1,96 @@
+package com.mealtiger.backend.database.repository;
+
+import com.mealtiger.backend.BackendApplication;
+import com.mealtiger.backend.database.model.recipe.Ingredient;
+import com.mealtiger.backend.database.model.recipe.Recipe;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = {BackendApplication.class}
+)
+class RecipeRepositoryTest {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @BeforeEach
+    @AfterEach
+    void beforeAfterEach() {
+        recipeRepository.deleteAll();
+    }
+
+    @ParameterizedTest(name = "#{index} - Case insensitivity test with randomcased string {0}")
+    @MethodSource("randomcasedStrings")
+    void findRecipesByTitleContainingIgnoreCaseTest(String randomcased){
+        Recipe[] testRecipes = {
+                new Recipe(
+                        "Gebrannte Mandeln",
+                        new Ingredient[]{
+                                new Ingredient(500, "Gramm", "Mandeln, geschält"),
+                                new Ingredient(200, "Gramm", "Zucker")
+                        },
+                        "TestDescription",
+                        3,
+                        5,
+                        15
+                ),
+                new Recipe(
+                        "Gebratene Cashewkerne",
+                        new Ingredient[]{
+                                new Ingredient(500, "Gramm", "Cashewkerne, geschält"),
+                                new Ingredient(200, "Gramm", "Zucker")
+                        },
+                        "TestDescription",
+                        3,
+                        5,
+                        15
+                ),
+                new Recipe(
+                        "Toast Hawaii",
+                        new Ingredient[]{
+                                new Ingredient(500, "Gramm", "Schinken"),
+                                new Ingredient(10, "Scheiben", "Toastbrot"),
+                                new Ingredient(10, "Scheiben", "Ananas"),
+                                new Ingredient(10, "Scheiben", "Schmelzkäse")
+                        },
+                        "TestDescription",
+                        1,
+                        4,
+                        15
+                )
+        };
+
+        recipeRepository.saveAll(Arrays.asList(testRecipes));
+
+        Pageable pageable = PageRequest.of(0,1);
+
+        String compareTitle = "Gebrannte Mandeln";
+        String foundTitle = recipeRepository.findRecipesByTitleContainingIgnoreCase(randomcased, pageable).get(0).getTitle();
+        assertEquals(compareTitle, foundTitle);
+    }
+
+    //Test case sensitivity
+
+    private static Stream<String> randomcasedStrings() {
+        return Stream.of(
+                "GEBRaNNTe mandEln",
+                "gEbRaNNTe MANDElN",
+                "GeBrAnNTe mANdeln",
+                "gEBrANntE MAndEln",
+                "GEBrANnte mAnDELN"
+                );
+    }
+}
