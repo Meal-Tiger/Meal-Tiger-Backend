151a128b5a758855c5bfdfc87c7cc399a1c12190
Uggah <luccagreschner@gmail.com>
2022-12-05 21:52:19 +0100

    Fix imports

--- src/main/java/com/mealtiger/backend/BackendApplication.java
+++ src/main/java/com/mealtiger/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
