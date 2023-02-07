f36ad83374a601217667515889f005024ce6cbcc
Uggah <luccagreschner@gmail.com>
2023-02-07 21:36:39 +0100

    Add unit test for saving image

--- src/test/java/com/mealtiger/backend/rest/controller/ImageIOControllerTest.java
+++ src/test/java/com/mealtiger/backend/rest/controller/ImageIOControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 
 import java.awt.image.BufferedImage;
@@ -61,6 +62,23 @@ class ImageIOControllerTest {
         if(Files.exists(imagePath)) {
             Helper.deleteFile(imagePath);
         }
+    }
+
+    /**
+     * Tests reading images.
+     */
+    @Test
+    void readImageTest() throws IOException {
+        when(configurator.getString("Image.imagePath")).thenReturn("testImages/");
+
+        ImageIOController controller = new ImageIOController(bitmapAdapter, gifAdapter, jpegAdapter, pngAdapter, webPAdapter, configurator, imageMetadataRepository);
+
+        MockMultipartFile multipartFile = spy(new MockMultipartFile("file", this.getClass().getClassLoader().getResourceAsStream("com/mealtiger/backend/imageio/testImages/DefaultTestImage/TestImage.jpg")));
+        BufferedImage image = controller.readImage(multipartFile);
+
+        verify(multipartFile).getInputStream();
+        assertNotEquals(0, image.getHeight());
+        assertNotEquals(0, image.getWidth());
     }
 
     /**
