82b2127e49008ee2b1054cac12fa4af263d1289a
Uggah <luccagreschner@gmail.com>
2023-02-10 22:37:14 +0100

    Add more unit tests

--- src/test/java/com/mealtiger/backend/database/model/recipe/RatingTest.java
+++ src/test/java/com/mealtiger/backend/database/model/recipe/RatingTest.java
@@ -6,8 +6,7 @@ import com.mealtiger.backend.rest.model.rating.RatingResponse;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("unit")
 class RatingTest {
@@ -34,6 +33,24 @@ class RatingTest {
         assertEquals(ratingRequest.getRatingValue(), rating.getRatingValue());
         assertEquals(ratingRequest.getComment(), rating.getComment());
         assertNull(rating.getUserId());
+    }
+
+    @Test
+    void equalsHashCodeTest() {
+        Rating reference = new Rating(SampleSource.SAMPLE_RATING_ID, 2, "Some comment", SampleSource.SAMPLE_USER_ID);
+        Rating differentId = new Rating(SampleSource.getSampleUUIDs().get(0), 2, "Some comment", SampleSource.SAMPLE_USER_ID);
+        Rating differentRatingValue = new Rating(SampleSource.SAMPLE_RATING_ID, 3, "Some comment", SampleSource.SAMPLE_USER_ID);
+        Rating differentUserId = new Rating(SampleSource.SAMPLE_RATING_ID, 2, "Some comment", SampleSource.SAMPLE_OTHER_USER_ID);
+
+        assertEquals(reference, reference);
+        assertEquals(reference, differentId);
+        assertNotEquals(reference, differentRatingValue);
+        assertNotEquals(reference, differentUserId);
+
+        assertEquals(reference.hashCode(), reference.hashCode());
+        assertEquals(reference.hashCode(), differentId.hashCode());
+        assertNotEquals(reference.hashCode(), differentRatingValue.hashCode());
+        assertNotEquals(reference.hashCode(), differentUserId.hashCode());
     }
 
 }
