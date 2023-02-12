fa4c4fe2af92cea00e14af60705589dbfc051dd9
Uggah <luccagreschner@gmail.com>
2023-02-12 11:41:38 +0100

    Extend thoroughness of unit tests

--- src/test/java/com/mealtiger/backend/database/model/user/UserMetadataTest.java
+++ src/test/java/com/mealtiger/backend/database/model/user/UserMetadataTest.java
@@ -1,12 +1,13 @@
 package com.mealtiger.backend.database.model.user;
 
+import com.mealtiger.backend.SampleSource;
 import com.mealtiger.backend.rest.model.user.UserInformationResponse;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.mealtiger.backend.SampleSource.SAMPLE_IMAGE_ID;
-import static com.mealtiger.backend.SampleSource.SAMPLE_USER_ID;
+import static com.mealtiger.backend.SampleSource.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @Tag("unit")
 class UserMetadataTest {
@@ -22,5 +23,26 @@ class UserMetadataTest {
         assertEquals(userMetadata.getUserId(), userInformationResponse.getUserId());
         assertEquals(userMetadata.getUsername(), userInformationResponse.getUsername());
         assertEquals(userMetadata.getProfilePictureId(), userInformationResponse.getProfilePictureId());
+    }
+
+    /**
+     * Tests the equals- and hashCode-methods
+     */
+    @Test
+    void equalsHashCodeTest() {
+        UserMetadata reference = new UserMetadata(SAMPLE_USER_ID, "Sample username", SAMPLE_IMAGE_ID);
+        UserMetadata differentUserID = new UserMetadata(SAMPLE_OTHER_USER_ID, "Sample username", SAMPLE_IMAGE_ID);
+        UserMetadata differentUsername = new UserMetadata(SAMPLE_USER_ID, "Sample other username", SAMPLE_IMAGE_ID);
+        UserMetadata differentImageID = new UserMetadata(SAMPLE_USER_ID, "Sample username", SampleSource.getSampleUUIDs().get(0));
+
+        assertEquals(reference, reference);
+        assertNotEquals(reference, differentUserID);
+        assertNotEquals(reference, differentUsername);
+        assertNotEquals(reference, differentImageID);
+
+        assertEquals(reference.hashCode(), reference.hashCode());
+        assertNotEquals(reference.hashCode(), differentUserID.hashCode());
+        assertNotEquals(reference.hashCode(), differentUsername.hashCode());
+        assertNotEquals(reference.hashCode(), differentImageID.hashCode());
     }
 }
